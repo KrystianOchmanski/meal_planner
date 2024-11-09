9ab259b19c73d8d69c37d42e9dd80b8b0d58bf10
Krystian Ochmański <krysochmanski1337@gmail.com>
2024-11-09 17:28:12 +0100

    Added calendar from pub.dev. Thinking about making my own

--- lib/shopping_list_screen.dart
+++ lib/shopping_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meal_planner/data/database.dart';
 import 'package:meal_planner/data/models/shopping_list_item_with_details.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class ShoppingListScreen extends StatefulWidget {
   const ShoppingListScreen({super.key});
@@ -18,28 +20,60 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
   }
 }
 
-class ProductListScreen extends StatelessWidget {
-  final db = AppDatabase.instance;
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key});
 
-  ProductListScreen({super.key});
+  @override
+  ProductListScreenState createState() => ProductListScreenState();
+}
+
+class ProductListScreenState extends State<ProductListScreen> {
+  final db = AppDatabase.instance;
+  DateTimeRange? selectedDateRange;
+
+  Future<void> _selectDateRange(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Wybierz zakres dat'),
+          content: SizedBox(
+            width: 300,
+            height: 300,
+            child: SfDateRangePicker(
+              selectionMode: DateRangePickerSelectionMode.range,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Anuluj', style: GoogleFonts.poppins(color: Colors.red)),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Generuj', style: GoogleFonts.poppins()),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista Produktów'),
-      ),
       body: Column(
         children: [
+          SizedBox(height: 10),
           FutureBuilder<List<ShoppingListItemWithDetails>>(
             future: db.getShoppingListItemsWithDetails(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
+                return Center(child: CircularProgressIndicator());
               } else if (snapshot.hasError) {
-                return Text('Błąd: ${snapshot.error}');
+                return Center(child: Text('Błąd: ${snapshot.error}'));
               } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Text('Brak produktów');
+                return Center(child: Text('Brak produktów', style: TextStyle(color: Colors.grey)));
               } else {
                 final items = snapshot.data!;
                 return ListView.builder(
@@ -58,10 +92,13 @@ class ProductListScreen extends StatelessWidget {
           Expanded(
             child: Center(
               child: ElevatedButton(
-                  onPressed: null, 
-                  child: Text('Generuj')),
+                onPressed: () => _selectDateRange(context),
+                child: Text(selectedDateRange == null
+                    ? 'Generuj listę zakupów'
+                    : 'Wybrano: ${selectedDateRange!.start} - ${selectedDateRange!.end}'),
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
